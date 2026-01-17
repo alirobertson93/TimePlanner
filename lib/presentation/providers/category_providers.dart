02afca560ce8d058307f5a8d23c6407fbf4cffcc
alirobertson93 <alirobertson93@gmail.com>
2026-01-17 01:32:38 +0000

    Create category_providers.dart file

---
+++ lib/presentation/providers/category_providers.dart
@@ -0,0 +1,19 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../../domain/entities/category.dart';
+import 'repository_providers.dart';
+
+part 'category_providers.g.dart';
+
+/// Provider for all categories
+@riverpod
+Future<List<Category>> categories(Ref ref) async {
+  final repository = ref.watch(categoryRepositoryProvider);
+  return repository.getAll();
+}
+
+/// Provider to get a single category by ID
+@riverpod
+Future<Category?> categoryById(Ref ref, String categoryId) async {
+  final repository = ref.watch(categoryRepositoryProvider);
+  return repository.getById(categoryId);
+}
